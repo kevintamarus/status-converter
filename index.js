23bb607c431814685bb02bed1afa537f1c05f7a2
Kevin Tamarus Tang <kevintamarus@gmail.com>
2017-11-20 12:31:11 -0800

    forgot to handle null cases, added

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 module.exports = {
   convertStatus: function(status) {
-    if(status.toLowerCase() === 'failed') {
-      return 'declined';
-    }
-    if(status.toLowerCase() === 'completed') {
-      return 'approved';
+    if(status) {
+      if(status.toLowerCase() === 'failed') {
+        return 'declined';
+      }
+      if(status.toLowerCase() === 'completed') {
+        return 'approved';
+      }
     }
     return status;
   },
   convertTransactionMessage: function(message) {
-    if(message.includes('failed')) {
-      return message.replace(new RegExp('failed'), 'DECLINED');
-    }
-    if(message.includes('completed')) {
-      return message.replace(new RegExp('completed'), 'APPROVED');
+    if(status) {
+      if(message.includes('failed')) {
+        return message.replace(new RegExp('failed'), 'DECLINED');
+      }
+      if(message.includes('completed')) {
+        return message.replace(new RegExp('completed'), 'APPROVED');
+      }
     }
     return message;
   }
